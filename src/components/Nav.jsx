e77c8ffea164339a6b7264d2ced1aec43c21cbdf
ispm19 <ispm19@hotmail.com>
2024-01-01 16:48:00 -0400

    hola
    hola

---
+++ src/components/Nav.jsx
@@ -0,0 +1,189 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="#"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          > 
+            <svg
+  version="1.1"
+  id="Layer_1"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlnsXlink="http://www.w3.org/1999/xlink"
+  viewBox="0 0 512 512"
+  xmlSpace="preserve"
+  width="45px"
+  height="45px"
+  fill="#000000"
+>
+  <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+  <g id="SVGRepo_iconCarrier">
+    {" "}
+    <rect
+      x="249.405"
+      y="56.208"
+      style={{ fill: "#F7B239" }}
+      width="252.602"
+      height="265.828"
+    />{" "}
+    <rect
+      x="249.405"
+      y="13.586"
+      style={{ fill: "#2BA5F7" }}
+      width="252.602"
+      height="42.622"
+    />{" "}
+    <rect
+      x="9.99"
+      y="360.422"
+      style={{ fill: "#E6E6E6" }}
+      width="492.017"
+      height="137.989"
+    />{" "}
+    <rect
+      x="9.99"
+      y="13.588"
+      style={{ fill: "#2197D8" }}
+      width="193.677"
+      height="308.45"
+    />{" "}
+    <g>
+      {" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M502.01,350.434H9.99c-5.517,0-9.99,4.473-9.99,9.99v137.989c0,5.517,4.473,9.99,9.99,9.99H502.01 c5.517,0,9.99-4.473,9.99-9.99V360.424C512,354.907,507.527,350.434,502.01,350.434z M492.021,396.431H453.4v-26.017h38.621 V396.431z M19.979,442.427v-26.018h15.622v26.018H19.979z M55.58,416.41h42.491v26.018H55.58V416.41z M390.93,396.431v-26.017 h42.491v26.017H390.93z M367.931,442.427v-26.018h42.491v26.018H367.931z M305.461,442.427v-26.018h42.491v26.018H305.461z M180.521,442.427v-26.018h42.491v26.018H180.521z M118.051,442.427v-26.018h42.49v26.018H118.051z M242.992,416.41h42.491v26.018 h-42.491V416.41z M370.951,396.431h-42.491v-26.017h42.491V396.431z M308.48,396.431h-42.49v-26.017h42.49V396.431z M246.011,396.431H203.52v-26.017h42.491V396.431z M183.541,396.431h-42.491v-26.017h42.491V396.431z M121.07,396.431H78.579 v-26.017h42.491L121.07,396.431L121.07,396.431z M121.07,462.406v26.017H78.579v-26.017H121.07z M141.049,462.406h42.491v26.017 h-42.491V462.406z M203.52,462.406h104.96v26.017H203.52V462.406z M328.459,462.406h42.491v26.017h-42.491V462.406z M390.93,462.406h42.491v26.017H390.93V462.406z M430.402,442.427v-26.018h26.017v26.018H430.402z M476.397,416.41h15.622v26.018 h-15.622V416.41z M58.6,370.413v26.017H19.979v-26.017H58.6z M19.979,462.406H58.6v26.017H19.979V462.406z M453.4,488.423v-26.017 h38.621v26.017H453.4z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M9.99,332.026h193.673c5.517,0,9.99-4.473,9.99-9.99V13.588c0-5.517-4.473-9.99-9.99-9.99H9.99 C4.473,3.598,0,8.071,0,13.588v308.448C0,327.553,4.473,332.026,9.99,332.026z M19.979,23.577h173.694v288.469H19.979V23.577z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M502.01,3.598H249.405c-5.517,0-9.99,4.473-9.99,9.99v308.448c0,5.517,4.473,9.99,9.99,9.99h252.604 c5.517,0,9.99-4.473,9.99-9.99V13.588C512,8.071,507.527,3.598,502.01,3.598z M492.021,23.577V46.22H259.394V23.577H492.021z M259.394,312.046V66.199H492.02v245.847H259.394z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M161.435,53.798H52.216c-5.517,0-9.99,4.473-9.99,9.99s4.473,9.99,9.99,9.99h109.219 c5.517,0,9.99-4.473,9.99-9.99S166.951,53.798,161.435,53.798z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M52.216,115.388h54.672c5.517,0,9.99-4.473,9.99-9.99s-4.473-9.99-9.99-9.99H52.216 c-5.517,0-9.99,4.473-9.99,9.99S46.699,115.388,52.216,115.388z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M161.435,137.017H52.216c-5.517,0-9.99,4.473-9.99,9.99s4.473,9.99,9.99,9.99h109.219 c5.517,0,9.99-4.473,9.99-9.99S166.951,137.017,161.435,137.017z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M52.216,198.607h54.672c5.517,0,9.99-4.473,9.99-9.99s-4.473-9.99-9.99-9.99H52.216 c-5.517,0-9.99,4.473-9.99,9.99S46.699,198.607,52.216,198.607z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M161.435,220.236H52.216c-5.517,0-9.99,4.473-9.99,9.99s4.473,9.99,9.99,9.99h109.219 c5.517,0,9.99-4.473,9.99-9.99S166.951,220.236,161.435,220.236z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M106.888,261.847H52.216c-5.517,0-9.99,4.473-9.99,9.99s4.473,9.99,9.99,9.99h54.672 c5.517,0,9.99-4.473,9.99-9.99S112.405,261.847,106.888,261.847z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M335.152,138.783c-3.901-3.901-10.227-3.901-14.127,0l-38.614,38.614 c-3.901,3.901-3.901,10.227,0,14.127l38.614,38.614c1.95,1.95,4.507,2.926,7.063,2.926s5.113-0.975,7.063-2.926 c3.901-3.901,3.901-10.227,0-14.127l-31.551-31.551l31.551-31.551C339.055,149.01,339.055,142.684,335.152,138.783z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M430.39,138.783c-3.901-3.901-10.227-3.901-14.126,0c-3.901,3.901-3.901,10.227,0,14.127 l31.551,31.551l-31.551,31.551c-3.901,3.901-3.901,10.227,0,14.127c1.95,1.95,4.507,2.926,7.063,2.926s5.113-0.975,7.063-2.926 l38.614-38.614c3.901-3.901,3.901-10.227,0-14.127L430.39,138.783z"
+      />{" "}
+      <path
+        style={{ fill: "#333333" }}
+        d="M390.015,110.84c-5.352-1.347-10.778,1.905-12.122,7.255l-23.747,94.566 c-1.344,5.352,1.905,10.778,7.255,12.122c0.816,0.205,1.636,0.304,2.44,0.304c4.471,0,8.543-3.025,9.682-7.559l23.747-94.566 C398.614,117.611,395.365,112.184,390.015,110.84z"
+      />{" "}
+    </g>{" "}
+  </g>
+</svg>
+   
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Contact me!
+            </button>
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-cta"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+    );
+}
+
+
+export default Nav;
