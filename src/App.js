f9e18ef9271623eaf018ff953e895153256a3827
ispm19 <ispm19@hotmail.com>
2024-01-04 15:52:04 -0400

    footer
    Footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from './components/Nav';
 import Banner from './components/Banner';
 import Projects from './components/Projects';
+import Footer1 from './components/Footer1';
 import {NextUIProvider} from "@nextui-org/react";
 import './App.css';
 
@@ -12,6 +13,7 @@ const App = () => {
     <Nav />
     <Banner />
     <Projects />
+    <Footer1/>
     </>
     </NextUIProvider>
   
